a3013b16a892fff92727d8614abe69e928e763bb
prasanna <ror.prasanna@gmail.com>
2021-03-16 22:00:46 +0530

    article routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}
+    resources :articles do
+    end
   end
 
   get '/' => 'home#index'
-
-  resources :articles do
-    
-  end
 end
